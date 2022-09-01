74420640c32f9fbf226af55cbfb8ce1e1b74ddd4
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-01 14:09:01 +0700

    create admin seed

---
+++ database/seeders/AdminSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(!Admin::where('username','admin')->first()) {
+            $admin = new Admin;
+            $admin->username = 'admin';
+            $admin->password = bcrypt('123');
+            $admin->save();
+        }
+    }
+}
