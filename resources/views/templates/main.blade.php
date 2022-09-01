97e66fa91fecc88ed3c3b5b04d2c2557e218a107
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-01 13:07:03 +0700

    apply templating

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -34,7 +34,7 @@
           @include('templates.header')
           
           <div class="container-fluid">
-            <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+            @yield('content')
           </div>
         </div>
 
