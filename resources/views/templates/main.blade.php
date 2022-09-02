f3675827b2eac06ade94be1690318eb5bed301a6
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-02 11:43:22 +0700

    import custom css

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -10,6 +10,7 @@
     <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/custom.css')}}" rel="stylesheet">
 
     <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -52,7 +53,7 @@
           </div>
         </div>
 
-        <footer class="sticky-footer bg-white">
+        <footer class="sticky-footer bg-white mt-5">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
               <span>Copyright &copy; Your Website 2020</span>
