a1378779eba120313d55ed7e4901658f5fc76fd2
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-05 08:18:51 +0700

    daterange picker

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -10,11 +10,14 @@
     <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/daterangepicker.css')}}" rel="stylesheet">
     <link href="{{asset('assets/css/custom.css')}}" rel="stylesheet">
 
     <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('assets/js/moment.min.js')}}"></script>
+    <script src="{{asset('assets/js/daterangepicker.min.js')}}"></script>
   </head>
   <body id="page-top">
     <div id="wrapper">
@@ -65,5 +68,16 @@
     </div>
     
     <script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
+    <script>
+      $(function(){
+        $('#dtRangePicker').daterangepicker({
+          startDate: `{{ \Carbon\Carbon::now()->startOfMonth()->format('Y-m-d') }}`,
+          endDate: `{{ \Carbon\Carbon::now()->endOfMonth()->format('Y-m-d') }}`,
+          locale: {
+            format: 'Y-MM-DD'
+          }
+        })
+      })
+    </script>
   </body>
 </html>
