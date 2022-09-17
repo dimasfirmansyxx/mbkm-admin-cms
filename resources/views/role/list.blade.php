23b42b57d7455b757738dce6a5526886d80e27b1
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-17 08:59:33 +0700

    update url

--- resources/views/role/list.blade.php
+++ resources/views/role/list.blade.php
@@ -6,7 +6,7 @@
 <div class="card shadow">
   <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
     <h6 class="m-0 font-weight-bold text-primary">Role List</h6>
-    <a href="/role/form" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i></a>
+    <a href="/authorization/role/form" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i></a>
   </div>
   <div class="card-body">
     <table class="table table-bordered table-hover table-striped">
@@ -24,8 +24,8 @@
               <td class="text-center">{{ $loop->iteration }}</td>
               <td>{{ $row->name }}</td>
               <td width="100">
-                <a href="/role/form?id={{ $row->id }}" class="btn btn-warning btn-sm btn-icon"><i class="fas fa-pen"></i></a>
-                <a href="/role/delete?id={{ $row->id }}" onclick="return confirm('Are you sure want to delete this role?')" class="btn btn-danger btn-sm btn-icon"><i class="fas fa-trash"></i></a>
+                <a href="/authorization/role/form?id={{ $row->id }}" class="btn btn-warning btn-sm btn-icon"><i class="fas fa-pen"></i></a>
+                <a href="/authorization/role/delete?id={{ $row->id }}" onclick="return confirm('Are you sure want to delete this role?')" class="btn btn-danger btn-sm btn-icon"><i class="fas fa-trash"></i></a>
               </td>
             </tr>
           @endforeach
