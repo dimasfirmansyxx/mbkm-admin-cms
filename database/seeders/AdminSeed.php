085a4f9b13bf32b498fb055d890b4cdee3a007cc
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-20 13:35:40 +0700

    fix admin role seed with role

--- database/seeders/AdminSeed.php
+++ database/seeders/AdminSeed.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Admin;
+use App\Models\Role;
 
 class AdminSeed extends Seeder
 {
@@ -19,7 +20,7 @@ class AdminSeed extends Seeder
             $admin = new Admin;
             $admin->username = 'admin';
             $admin->password = bcrypt('123');
-            $admin->role = Role::where('name','admin')->first()->id;
+            $admin->role_id = Role::where('name','admin')->first()->id;
             $admin->save();
         }
     }
