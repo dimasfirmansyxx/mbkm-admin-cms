0344e964abfa028de2e4ea383262bc093cfcb4b8
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-20 10:42:21 +0700

    apply permission middleware to route

--- routes/web.php
+++ routes/web.php
@@ -30,63 +30,73 @@ Route::prefix('auth')->group(function(){
     Route::get('/logout',[AuthController::class, 'logout'])->middleware('auth');
 });
 
-Route::middleware('auth')->group(function(){
+Route::middleware(['auth','permission'])->group(function(){
     Route::get('/',function(){
         return view('dashboard');
     });
 
     Route::prefix('/product')->group(function(){
-        Route::get('/',[ProductController::class, 'productList']);
-        Route::get('/form',[ProductController::class, 'productForm']);
-        Route::post('/form',[ProductController::class, 'productSave']);
-        Route::get('/delete',[ProductController::class, 'productDelete']);
+        Route::get('/',[ProductController::class, 'productList'])->name('product|view');
+        Route::get('/form',[ProductController::class, 'productForm'])->name('product|add');
+        Route::get('/form/{id}',[ProductController::class, 'productForm'])->name('product|edit');
+        Route::post('/form',[ProductController::class, 'productSave'])->name('product|add');
+        Route::post('/form/{id}',[ProductController::class, 'productSave'])->name('product|edit');
+        Route::get('/delete',[ProductController::class, 'productDelete'])->name('product|delete');
 
         Route::prefix('/category')->group(function(){
-            Route::get('/',[ProductController::class, 'categoryList']);
-            Route::get('/form',[ProductController::class, 'categoryForm']);
-            Route::post('/form',[ProductController::class, 'categorySave']);
-            Route::get('/delete',[ProductController::class, 'categoryDelete']);
+            Route::get('/',[ProductController::class, 'categoryList'])->name('product_category|view');
+            Route::get('/form',[ProductController::class, 'categoryForm'])->name('product_category|add');
+            Route::get('/form/{id}',[ProductController::class, 'categoryForm'])->name('product_category|edit');
+            Route::post('/form',[ProductController::class, 'categorySave'])->name('product_category|add');
+            Route::post('/form/{id}',[ProductController::class, 'categorySave'])->name('product_category|edit');
+            Route::get('/delete',[ProductController::class, 'categoryDelete'])->name('product_category|delete');
         });
     });
 
     Route::prefix('/voucher')->group(function(){
-        Route::get('/',[VoucherController::class, 'list']);
-        Route::get('/form',[VoucherController::class, 'form']);
-        Route::post('/form',[VoucherController::class, 'save']);
-        Route::get('/delete',[VoucherController::class, 'delete']);
+        Route::get('/',[VoucherController::class, 'list'])->name('voucher|view');
+        Route::get('/form',[VoucherController::class, 'form'])->name('voucher|add');
+        Route::get('/form/{id}',[VoucherController::class, 'form'])->name('voucher|edit');
+        Route::post('/form',[VoucherController::class, 'save']->name('voucher|add'));
+        Route::post('/form/{id}',[VoucherController::class, 'save']->name('voucher|edit'));
+        Route::get('/delete',[VoucherController::class, 'delete'])->name('voucher|delete');
     });
 
     Route::prefix('/trx')->group(function(){
-        Route::get('/',[TransactionController::class, 'list']);
-        Route::get('/create',[TransactionController::class, 'create']);
-        Route::post('/create',[TransactionController::class, 'createTrx']);
-        Route::get('/update/{id}',[TransactionController::class, 'update']);
-        Route::post('/update/{id}',[TransactionController::class, 'updateTrx']);
-        Route::get('/paid',[TransactionController::class, 'setPaid']);
-        Route::get('/cancel',[TransactionController::class, 'setCancel']);
-        Route::get('/delete',[TransactionController::class, 'delete']);
+        Route::get('/',[TransactionController::class, 'list'])->name('transaction|view');
+        Route::get('/create',[TransactionController::class, 'create'])->name('transaction|add');
+        Route::post('/create',[TransactionController::class, 'createTrx'])->name('transaction|add');
+        Route::get('/update/{id}',[TransactionController::class, 'update'])->name('transaction|edit');
+        Route::post('/update/{id}',[TransactionController::class, 'updateTrx'])->name('transaction|edit');
+        Route::get('/paid',[TransactionController::class, 'setPaid'])->name('transaction|edit');
+        Route::get('/cancel',[TransactionController::class, 'setCancel'])->name('transaction|edit');
+        Route::get('/delete',[TransactionController::class, 'delete'])->name('transaction|delete');
         Route::get('/product/{id}',[TransactionController::class, 'getProduct']);
         Route::post('/voucher',[TransactionController::class, 'claimVoucher']);
     });
 
     Route::prefix('/authorization')->group(function(){
         Route::prefix('/role')->group(function(){
-            Route::get('/',[RoleController::class, 'list']);
-            Route::get('/form',[RoleController::class, 'form']);
-            Route::post('/form',[RoleController::class, 'save']);
-            Route::get('/delete',[RoleController::class, 'delete']);
+            Route::get('/',[RoleController::class, 'list'])->name('role|view');
+            Route::get('/form',[RoleController::class, 'form'])->name('role|add');
+            Route::get('/form',[RoleController::class, 'form'])->name('role|edit');
+            Route::post('/form',[RoleController::class, 'save'])->name('role|add');
+            Route::post('/form',[RoleController::class, 'save'])->name('role|edit');
+            Route::get('/delete',[RoleController::class, 'delete'])->name('role|delete');
         });
 
         Route::prefix('/user')->group(function(){
-            Route::get('/',[UserController::class, 'list']);
-            Route::get('/form',[UserController::class, 'form']);
-            Route::post('/form',[UserController::class, 'save']);
-            Route::get('/delete',[UserController::class, 'delete']);
+            Route::get('/',[UserController::class, 'list'])->name('user|view');
+            Route::get('/form',[UserController::class, 'form'])->name('user|add');
+            Route::get('/form',[UserController::class, 'form'])->name('user|edit');
+            Route::post('/form',[UserController::class, 'save'])->name('user|add');
+            Route::post('/form',[UserController::class, 'save'])->name('user|edit');
+            Route::get('/delete',[UserController::class, 'delete'])->name('user|delete');
         });
 
         Route::prefix('/permission')->group(function(){
-            Route::get('/',[PermissionController::class, 'list']);
-            Route::post('/',[PermissionController::class, 'save']);
+            Route::get('/',[PermissionController::class, 'list'])->name('user|view');
+            Route::post('/',[PermissionController::class, 'save'])->name('user|edit');
         });
     });
 });
